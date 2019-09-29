2c21a907b9a7d60e383f8446bb2701889e6028f4
unknown <Administrator@HI-09317.imr.no>
2019-09-29 21:26:55 +0200

    Fixed typo

--- R/debuggingWin.R
+++ R/debuggingWin.R
@@ -1,6 +1,6 @@
 ## In order to run gdbsource on the R script, run this script:
 
-## If running under 64 bit Windows, first run this tomake sure 
+## If running under 64 bit Windows, first run this to make sure 
 ## the directories are in the corect order:
 
 fixwinpath <- function() {
@@ -15,5 +15,5 @@ fixwinpath()
 ## Then load TMB and run the script:
 
 library(TMB)
-gdbsource('./R/harps_and_hoods_population_model3.R', interactive=T)
+gdbsource('./R/harps_and_hoods_population_model4.R', interactive=T)
 
